2e4b2a91b7f77b353e6219f7d9f9158c7acc33c6
jiapeidi <jiapeidi@sunniwell.net>
2017-12-10 11:36:36 +0800

    Init Reposity

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# AllTestDemo
+记录所有的测试代码的Demo
+==============
+### 包含的Demo列表：
+ * 注解去除繁琐的`findViewById`、`setContentView`、`setOnClickListener`
+ * 主线程更新UI不报错的接口 [原文链接](https://juejin.im/post/5a1a58f251882540f363419c)
+ * 自定义View的canvas和paint各个接口的测试(**未完待续**）
+ * 2014 Google I/O之Material Design
+     * SnackBar的接口使用
---
+++ app/src/main/java/net/george/alltestdemo/MainActivity.java
@@ -0,0 +1,18 @@
+package net.george.alltestdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 此项目用来纪录所有的练习Demo，主要是为了加强和巩固Android各个领域的知识点
+ * Created at 2017.12.10 By George
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
