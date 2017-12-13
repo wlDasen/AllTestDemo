ee4685adb5b3fcf1f8ada5f99e94f674f707030e
wlDasen <jiapeidi@sunniwell.net>
2017-12-13 16:39:13 +0800

    添加ConstraintLayout测试Demo

---
+++ app/src/main/java/net/george/alltestdemo/ConstraintLayoutActivity.java
@@ -0,0 +1,21 @@
+package net.george.alltestdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * ConstraintLayout验证Activity
+ * Created at 2017.12.12 By George
+ */
+public class ConstraintLayoutActivity extends AppCompatActivity {
+    private static final String TAG = "jpd-CLAc";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_layout);
+    }
+}
--- app/src/main/java/net/george/alltestdemo/MainActivity.java
+++ app/src/main/java/net/george/alltestdemo/MainActivity.java
@@ -3,6 +3,7 @@ package net.george.alltestdemo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,6 +13,7 @@ import android.widget.Button;
  */
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "jpd-Mac";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
